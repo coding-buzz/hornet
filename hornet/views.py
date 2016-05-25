b55368955a214a53bf20adb834939cea2eb35dd4
Marcin Skiba <marcinskiba91@gmail.com>
2016-05-25 17:39:32 +0200

    Add social links with styles, add navbar links styles.

--- hornet/views.py
+++ hornet/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
+    social_links = [
+        {'name': 'cines91', 'link': 'https://www.facebook.com/cines91', 'site': 'Facebook'},
+        {'name': 'ilu2112', 'link': 'https://github.com/ilu2112', 'site': 'GitHub'},
+        {'name': 'coding-buzz', 'link': 'https://github.com/coding-buzz', 'site': 'GitHub'},
+        {'name': 'marcinskiba91', 'link': 'https://www.linkedin.com/in/marcinskiba91', 'site': 'LinkedIn'},
+        {'name': 'marcin', 'link': 'mailto:marcin@coding.buzz', 'site': 'coding.buzz'}
+    ]
     skills = [
         {'name': 'Java SE/EE', 'level': 90},
         {'name': 'Java Web Frameworks', 'level': 90},
@@ -23,6 +30,9 @@ def home(request):
         {'name': 'Linux', 'level': 75},
         {'name': 'Application Design', 'level': 80},
         {'name': 'UI/UX Design', 'level': 35},
-
     ]
-    return render(request, 'pages/home.html', context={'skills': skills})
+    context = {
+        'social_links': social_links,
+        'skills': skills
+    }
+    return render(request, 'pages/home.html', context)
