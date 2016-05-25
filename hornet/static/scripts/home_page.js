8e10120a3d6eaf71509919880320bfba3376f7e7
Marcin Skiba <marcinskiba91@gmail.com>
2016-05-25 11:38:45 +0200

    Refactor the whole view layer.

--- hornet/static/scripts/home_page.js
+++ hornet/static/scripts/home_page.js
@@ -1,32 +1,62 @@
-$(function() {
-    var skillBars = $('.skill-bar').toArray();
+var homepage = {
+    skillBars: [],
+    expandedSkillBarClassName: 'expanded',
 
-    $(window).scroll(function() {
-        $(skillBars).each(function() {
-            if (isScrolledIntoView(this)) {
-                skillBars.splice(skillBars.indexOf(this), 1);
-                $(this).addClass('expanded');
-            }
+
+    init: function() {
+        this.initScrollBarsAnimation();
+        this.initImageSlider();
+    },
+
+    initScrollBarsAnimation: function() {
+        this.skillBars = $('.skill-bar').toArray();
+
+        $(window).scroll(function() {
+            this.expandVisibleSkillBars();
+            this.removeExpandedSkillBarsFromList();
+        }.bind(this));
+    },
+
+    initImageSlider: function() {
+        $('.companies-carousel').imageslider({
+            slideItems: '.image',
+            slideContainer: '.images-wrapper',
+            slideDuration: 15
         });
-    });
+    },
 
-    function isScrolledIntoView(elem) {
-        var $elem = $(elem);
+    expandVisibleSkillBars: function() {
+        $(this.skillBars).each(function(index, skillBar) {
+            this.expandSkillBarIfVisible(skillBar);
+        }.bind(this));
+    },
+
+    expandSkillBarIfVisible: function(skillBar) {
+        if (this.isScrolledIntoView(skillBar)) {
+            $(skillBar).addClass(this.expandedSkillBarClassName);
+        }
+    },
+
+    isScrolledIntoView: function(element) {
+        var $element = $(element);
         var $window = $(window);
-
         var docViewTop = $window.scrollTop();
         var docViewBottom = docViewTop + $window.height();
-
-        var elemTop = $elem.offset().top;
-        var elemBottom = elemTop + $elem.height();
-
+        var elemTop = $element.offset().top;
+        var elemBottom = elemTop + $element.height();
         return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    },
+
+    removeExpandedSkillBarsFromList: function() {
+        $(this.skillBars).each(function(index, skillBar) {
+            if ($(skillBar).hasClass(this.expandedSkillBarClassName)) {
+                this.skillBars.splice(this.skillBars.indexOf(skillBar), 1);
+            }
+        }.bind(this));
     }
+}
 
 
-    $('.companies-carousel').imageslider({
-        slideItems: '.image',
-        slideContainer: '.image-wrapper',
-        slideDuration: 2
-    });
+$(function() {
+    homepage.init();
 });
