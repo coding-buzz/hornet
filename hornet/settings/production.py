f5168b9482232c2bd029fdb9bc337b3d054bc88c
Marcin Skiba <marcinskiba91@gmail.com>
2016-09-14 12:51:46 +0200

    Optimize website.

--- hornet/settings/production.py
+++ hornet/settings/production.py
@@ -6,3 +6,9 @@ from base import *
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = ['coding.buzz']
+
+
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.cssmin.rCSSMinFilter',
+)
+COMPRESS_ENABLED = True
