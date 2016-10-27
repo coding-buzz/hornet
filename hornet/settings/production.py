0105709b095b664cee8af187774456c420aa777c
Marcin Skiba <marcinskiba91@gmail.com>
2016-10-27 11:13:07 +0200

    Minor UI improvements.

--- hornet/settings/production.py
+++ hornet/settings/production.py
@@ -7,8 +7,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = ['coding.buzz']
 
-
-COMPRESS_CSS_FILTERS += [
-    'compressor.filters.cssmin.rCSSMinFilter',
-]
+COMPRESS_CSS_FILTERS = (
+    'django_compressor_autoprefixer.AutoprefixerFilter',
+)
+COMPRESS_AUTOPREFIXER_BINARY = os.path.join(BASE_DIR, '..', 'node_modules/postcss-cli/bin/postcss')
+COMPRESS_AUTOPREFIXER_ARGS   = ' --use autoprefixer'
 COMPRESS_ENABLED = True
