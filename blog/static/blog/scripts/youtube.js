6d0836b01c5784265ce55efca67e8b1f10de8410
Marcin Skiba <marcinskiba91@gmail.com>
2016-11-11 13:49:48 +0100

    Add Youtube iframe.

---
+++ blog/static/blog/scripts/youtube.js
@@ -0,0 +1,14 @@
+$(function() {
+    console.log('start');
+    var iframeRatio = 0.5625;
+
+    function resizeYoutubeFrames() {
+        $('.yt-frame').each(function() {
+            var height = $(this).width() * iframeRatio;
+            $(this).css('height', height + 'px');
+        });
+    }
+
+    resizeYoutubeFrames();
+    $(window).resize(resizeYoutubeFrames);
+});
--- blog/templatetags/blog_post.py
+++ blog/templatetags/blog_post.py
@@ -10,6 +10,9 @@ register = template.Library()
 _IMAGE_TAG_REGEX = re.compile(r'{{ image \d+ }}')
 _IMAGE_HTML_TAG = '<img src="{path}" alt="{title}"/>'
 
+_YOUTUBE_TAG_REGEX = re.compile(r'{{ youtube .+ }}')
+_YOUTUBE_HTML_TAG = '<iframe class="yt-frame" type="text/html" src="http://www.youtube.com/embed/{movie_id}"></iframe>'
+
 
 @register.filter
 def include_images(post_content):
@@ -21,4 +24,23 @@ def include_images(post_content):
             post_content = post_content.replace(image_tag, image_html_tag)
         except ObjectDoesNotExist:
             pass
+    return post_content
+
+
+@register.filter
+def include_youtube(post_content):
+    for youtube_tag in re.findall(_YOUTUBE_TAG_REGEX, post_content):
+        try:
+            movie_id = youtube_tag.lstrip('{{ youtube ').rstrip(' }}')
+            youtube_html_tag = _YOUTUBE_HTML_TAG.format(movie_id=movie_id)
+            post_content = post_content.replace(youtube_tag, youtube_html_tag)
+        except ObjectDoesNotExist:
+            pass
+    return post_content
+
+
+@register.filter
+def include_all_elements(post_content):
+    post_content = include_images(post_content)
+    post_content = include_youtube(post_content)
     return post_content
