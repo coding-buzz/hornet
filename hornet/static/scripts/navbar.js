ebc42786a98b9faa1b8dc092018a98fec5280336
Marcin Skiba <marcinskiba91@gmail.com>
2016-11-12 17:38:01 +0100

    Adjust mini navbar threshold distance.

--- hornet/static/scripts/navbar.js
+++ hornet/static/scripts/navbar.js
@@ -1,6 +1,6 @@
 $(function() {
     var nav = $('nav');
-    var scrollDistanceThreshold = 55;
+    var scrollDistanceThreshold = 110;
 
     $(document).scroll(function() {
         var scrollDistance = $(this).scrollTop();
