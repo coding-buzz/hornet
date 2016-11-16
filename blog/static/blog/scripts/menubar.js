e1d4057d7574aaa536eb87b2693739a8e3f4d2ba
Marcin Skiba <marcinskiba91@gmail.com>
2016-11-16 11:33:34 +0100

    Add menubar to blog index page.

--- blog/static/blog/scripts/menubar.js
+++ blog/static/blog/scripts/menubar.js
@@ -7,4 +7,23 @@ $(function() {
         menubar.toggleClass('expanded');
         options.slideToggle();
     });
+
+    // iOS hover hack
+    options.find('.option-name').on('touchend', function(e) {
+        var parent = $(this).parents('li');
+        if (parent.hasClass('hovered')) {
+            parent.removeClass('hovered');
+        } else {
+            options.find('.hovered').removeClass('hovered');
+            $(this).parents('li').toggleClass('hovered');
+        }
+    });
+
+    options.find('> li').on('mouseenter', function() {
+        $(this).addClass('hovered');
+    });
+
+     options.find('> li').on('mouseleave', function() {
+        $(this).removeClass('hovered');
+    });
 });
