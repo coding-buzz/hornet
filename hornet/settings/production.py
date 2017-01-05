99bd708aaa37bc99426c2e84601fba0356ebaae2
Marcin Skiba <marcinskiba91@gmail.com>
2017-01-05 11:17:53 +0100

    Update production settings.

--- hornet/settings/production.py
+++ hornet/settings/production.py
@@ -4,8 +4,8 @@ from base import *
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+ALLOWED_HOSTS = ['coding.buzz']
 
 COMPRESS_CSS_FILTERS = (
     'django_compressor_autoprefixer.AutoprefixerFilter',
